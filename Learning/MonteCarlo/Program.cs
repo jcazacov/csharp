db60985f97e5c2a5672594fdfce3a99bd23a4931
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-08-09 13:10:37 +0200

    Monte Carlo Method to calculate Pi

---
+++ Learning/MonteCarlo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace MonteCarlo
+{
+    internal class Program
+    {
+        private const int N = 100000000;
+        private const int scale = 1000000;
+        
+        public static void Main(string[] args)
+        {
+            int index;
+            int inside = 0;
+            double pi;
+            Random rnd = new Random();
+
+            for (index = 0; index < N; index = index + 1)
+            {
+                double x = (double)rnd.Next(scale + 1) / scale;
+                double y = (double)rnd.Next(scale + 1) / scale;
+
+                if (x * x + y * y < 1)
+                {
+                    inside = inside + 1;
+                }
+                else
+                {
+                    
+                }
+
+                if ((index + 1) % 10000000 == 0)
+                {
+                    pi = ((double) inside / (double) index) * 4;
+                    Console.WriteLine(pi);
+                }
+                
+            }
+
+        }
+    }
+}
